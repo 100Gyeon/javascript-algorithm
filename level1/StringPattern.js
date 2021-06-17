bd4e9895382d1fff37dac83c404a179f278222cc
100Gyeon <bjy9966@naver.com>
2021-06-18 00:44:17 +0900

    Level 1) 수박수박수박수박수박수? - repeat, slice

--- level1/StringPattern.js
+++ level1/StringPattern.js
@@ -9,3 +9,10 @@ function solution(n) {
   }
   return answer;
 }
+
+// 더 짧은 솔루션
+// repeat() : 문자열을 주어진 횟수만큼 반복해 붙인 새로운 문자열을 반환
+// slice(begin, end) : begin 인덱스부터 end-1 인덱스까지 텍스트를 추출하고 새 문자열을 반환 
+function solution(n) {
+  return "수박".repeat(n).slice(0, n);
+}
