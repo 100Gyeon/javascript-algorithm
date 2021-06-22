622399e7764a5ae9f3587d05dd40876ac72a0889
100Gyeon <bjy9966@naver.com>
2021-06-22 17:43:26 +0900

    Level 1) 2016년 - new Date(y, m, d), getDay()

--- level1/Year.js
+++ level1/Year.js
@@ -6,3 +6,14 @@ function solution(a, b) {
 
 // new Date(y, m, d) : y, m, d는 각각 년, 월(0~11), 일(1~31)을 나타내며, 이 시간 정보를 가진 객체 생성
 // getDay() : 요일을 정수로 리턴, 일요일 = 0, 월요일 = 1
+
+// 다른 풀이
+function getDayName(a, b) {
+  var date = new Date(2016, a - 1, b);
+  return date.toString().slice(0, 3).toUpperCase();
+}
+/*
+new Date(y, m, d).toString()을 출력하면 Tue May 24 2016 00:00:00 GMT+0900 (KST)
+따라서 맨 앞의 요일 값을 slice 메서드로 잘라 출력한다.
+문제에서 제시한 것처럼 대문자로 출력하기 위해 .toUpperCase()를 사용한다.
+*/
