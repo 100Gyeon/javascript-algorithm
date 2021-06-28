9bd657a05e34698006fe4229a86a4fce1726d3b0
100Gyeon <bjy9966@naver.com>
2021-06-28 17:31:48 +0900

    Level 1) 평균 구하기 - Array.prototype.reduce()

--- level1/GetAverage.js
+++ level1/GetAverage.js
@@ -8,7 +8,7 @@ function solution(arr) {
 }
 
 // 더 짧은 솔루션
-// reduce() : 배열의 각 요소에 대해 주어진 리듀서(reducer) 함수를 실행하고, 하나의 결과값을 반환
+// reduce() : 배열의 각 요소에 대해 주어진 리듀서(reducer) 함수를 실행하고, 하나의 결괏값을 반환
 function solution(arr) {
     return arr.reduce((sum, curr) => sum + curr) / arr.length;
 }
