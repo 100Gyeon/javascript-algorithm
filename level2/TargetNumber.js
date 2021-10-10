8450ab148c3a20499fb96be1f297cdddd051f613
100Gyeon <bjy9966@naver.com>
2021-10-11 04:59:05 +0900

    Level 2) 타겟 넘버 - DFS

--- level2/TargetNumber.js
+++ level2/TargetNumber.js
@@ -2,17 +2,16 @@ const solution = (numbers, target) => {
   let answer = 0;
 
   const dfs = (depth, sum) => {
-    // leaf node까지 탐색한 후 target에 만족하는지 검사
-    if (depth === numbers.length) { // numbers 안을 모두 탐색했을 때
-      if (sum === target) { // sum과 target이 같으면 answer 증가
+    // leaf node까지 탐색한 후 target과 일치하는지 검사
+    if (depth === numbers.length) {
+      if (sum === target) {
         answer++;
       }
       return; // 가장 깊은 depth까지 도달했으므로 재귀에서 빠져나온다.
     }
 
-    // 숫자가 +로 시작하는 자식 노드를 탐색
+    // +,- 2가지로 가지쳐서 탐색
     dfs(depth + 1, sum + numbers[depth]);
-    // 숫자가 -로 시작하는 자식 노드를 탐색
     dfs(depth + 1, sum - numbers[depth]);
   }
 
