cc12f113c76bb976344b596114583e096af09ad0
100Gyeon <bjy9966@naver.com>
2021-12-16 19:31:51 +0900

    Level 3) 입국심사 - 이분탐색

--- level3/Immigration.js
+++ level3/Immigration.js
@@ -1,8 +1,10 @@
+/* 문제의 핵심 : 최솟값, 최댓값을 구한 뒤 이분탐색으로 범위를 좁혀가며 답을 구하는 것 */
 const solution = (n, times) => {
   // 이분탐색은 정렬되어 있는 상태에서 진행해야 한다.
   times.sort((a, b) => a - b);
   let min = 1;
   let max = n * times[times.length - 1];
+  // let max = n * Math.max(...times);도 가능
 
   while (min <= max) {
     let mid = Math.floor((min + max) / 2);
@@ -14,7 +16,7 @@ const solution = (n, times) => {
     });
 
     // 반복문을 돌며 심사할 수 있는 사람 수(people)와 기다리는 사람 수(n)를 비교
-    // 심사할 수 있는 사람 수 > 기다리는 사람 수 → 더 적은 시간 안에 해결 가능 → 시간 줄이기
+    // 심사할 수 있는 사람 수 >= 기다리는 사람 수 → 주어진 시간 안에 해결 가능 → 시간 줄이고 그 값으로도 가능한지 다시 검사
     // 심사할 수 있는 사람 수 < 기다리는 사람 수 → 더 많은 시간이 있어야 해결 가능 → 시간 늘리기
     people >= n ? max = mid - 1 : min = mid + 1;
   }
