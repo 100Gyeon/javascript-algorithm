019ad784efeea110af9f832a687846b5465016de
100Gyeon <bjy9966@naver.com>
2022-08-11 19:52:52 +0900

    Level 2) 땅따먹기 - Array.prototype.reduce(), DP

--- level2/Hopscotch.js
+++ level2/Hopscotch.js
@@ -1,3 +1,4 @@
+// Dynamic Programming (동적 계획법)
 function solution(land) {
   return Math.max(
     ...land.reduce(
@@ -14,7 +15,11 @@ function solution(land) {
   );
 }
 
-// 정확성, 효율성 통과 x
+/*
+정확성, 효율성 통과 x
+무조건 지금 상황에서의 최선을 선택하는 "탐욕법"으로 문제를 푼다면
+다음 줄에서 더 큰 숫자를 선택할 기회를 잃을 수 있기 때문에 다른 접근이 필요함.
+*/
 function solution(land) {
   let answer = 0;
   let maxIndex = land[0].indexOf(Math.max(...land[0]));
