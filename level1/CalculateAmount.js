3f092d629ad89f44114cd8c40c2f39f2dc8ead19
100Gyeon <bjy9966@naver.com>
2022-08-16 17:01:01 +0900

    Level 1) 부족한 금액 계산하기

---
+++ level1/CalculateAmount.js
@@ -0,0 +1,8 @@
+function solution(price, money, count) {
+  let totalPrice = 0;
+  for (let i = 1; i <= count; i++) {
+    totalPrice += price * i;
+  }
+  if (money > totalPrice) return 0;
+  return totalPrice - money;
+}
