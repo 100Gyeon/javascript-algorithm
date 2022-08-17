237558ca2650bc8fdb71448152ac565a2a93d6d2
100Gyeon <bjy9966@naver.com>
2022-08-18 00:47:46 +0900

    Level 2) 줄 서는 방법 - Array.prototype.reduce()로 팩토리얼 계산, Array.from()

--- level2/WayToStandInLine.js
+++ level2/WayToStandInLine.js
@@ -7,7 +7,7 @@ function solution(n, k) {
   }
   if (k === 1) return peopleList;
 
-  // k번째 방법은 배열의 index로 k-1에 해당
+  // k번째 방법은 배열[k-1]에 해당
   let index = k - 1;
   // 팩토리얼도 reduce로 구할 수 있음
   let factorial = peopleList.reduce((acc, cur) => acc * cur, 1);
@@ -22,3 +22,9 @@ function solution(n, k) {
 
   return answer;
 }
+
+/*
+Array.from()을 사용하면 반복문 없이도
+특정 길이의 배열을 만들어 연속된 수를 넣을 수 있다.
+let peopleList = Array.from({ length: n }, (_, i) => i + 1);
+*/
