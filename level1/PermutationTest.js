4757bdb76d4672794a70f8d9ab2beb685ba11d9b
100Gyeon <bjy9966@naver.com>
2022-08-18 21:09:37 +0900

    모의테스트) 순열 검사 - Array.prototype.every()

--- level1/PermutationTest.js
+++ level1/PermutationTest.js
@@ -15,16 +15,19 @@ function solution(arr) {
 }
 
 /*
-every() 메서드는 배열 안의 모든 요소가 주어진 판별 함수를 통과하는지 테스트한다. (Boolean 값을 반환)
+every() 메서드는 배열 안의 모든 요소가 주어진 판별 함수를 통과하는지 테스트한다.
+모든 배열 요소에 대해 참인 값을 반환하는 경우 true, 그 외는 false 반환.
 빈 배열에서 호출하면 무조건 true를 반환한다.
 every는 호출한 배열을 변형하지 않는다.
 
 [Syntax]
+// 화살표 함수
 every((element) => { ... } )
 every((element, index) => { ... } )
 every((element, index, array) => { ... } )
 
 [Example]
+// 모든 배열 요소의 크기 테스트
 [12, 5, 8, 130, 44].every(elem => elem >= 10); // false
 [12, 54, 18, 130, 44].every(elem => elem >= 10); // true
 */
