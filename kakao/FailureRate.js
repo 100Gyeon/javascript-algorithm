a8fa465e778c54a6d10f85d23714acda31625515
100Gyeon <bjy9966@naver.com>
2022-09-07 23:52:20 +0900

    [2019 KAKAO BLIND RECRUITMENT] 실패율

---
+++ kakao/FailureRate.js
@@ -0,0 +1,22 @@
+function solution(N, stages) {
+  let answer = [];
+  let length = stages.length;
+
+  stages.sort((a, b) => a - b);
+  const countMap = stages.reduce((acc, cur) => {
+    acc.set(cur, (acc.get(cur) || 0) + 1);
+    return acc;
+  }, new Map());
+
+  for (let [key, value] of countMap.entries()) {
+    if (key <= N) {
+      if (value === length) {
+        answer.push(key);
+        const temp = Array.from({ length: key - 1 }, (_, i) => i + 1);
+        answer = [...answer, ...temp];
+      }
+      length -= value;
+    }
+  }
+  return answer;
+}
